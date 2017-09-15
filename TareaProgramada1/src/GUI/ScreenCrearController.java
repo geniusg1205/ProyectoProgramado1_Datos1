84f1f2845549dc73564c12382731ec6893bef274
geniusg1205 <geniusg1205@gmail.com>
2017-09-15 13:09:48 -0600

    Actualizacion 15/9

--- TareaProgramada1/src/GUI/ScreenCrearController.java
+++ TareaProgramada1/src/GUI/ScreenCrearController.java
@@ -3,8 +3,6 @@ package GUI;
 import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,65 +20,19 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
-public class FXMLDocumentController  implements Initializable{
-	
-	@FXML
-	ScreensControll er mycontroller;
-	
-	@FXML
-	private Label label; 
-	@FXML
-	private ComboBox combo;
-	
-	@FXML
-	private TextField text;
-	
-	@FXML
-	private Button btn1;
-	
-	@FXML
-	TreeView <String> treeview;
-	
-	TreeItem <String> root = new TreeItem<>("Json Store");
-	
-	
-	ObservableList<String> listaCombo = FXCollections.observableArrayList();
-
-
-	@FXML
-	private void verItem(ActionEvent event) {
-		String itemSeleccionado = combo.getValue().toString();
-		text.setText(itemSeleccionado);
-		
-	}
-	
-	
-	@FXML
-	private void Exit(ActionEvent event) {
-		Platform.exit();	
-	}
-	
+public class ScreenCrearController  implements Initializable{
 	
 	@FXML
 	private void creaStore(ActionEvent event) {
 		Parent VentanaCrear = FXMLLoader.load(getClass().getResource("/GUI/ScreenCrear.fxml"));
 		Scene EscenaCrear = new Scene(VentanaCrear);
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
 	}
-	
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		TreeItem <String>  prueba = new TreeItem<>("Prueba");
-		root.getChildren().add(prueba);
-		treeview.setRoot(root);
 		
 		
-		
-		
-		
-	}
 
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+		
 }
